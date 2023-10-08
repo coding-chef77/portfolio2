029e47dcc08eadefd377b0a83c5f5e7cd5cff23a
Heine Günther <heine_gunther@hotmail.com>
2023-10-08 22:12:07 +0200

    small test with styled components

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,24 +1,53 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Hjem</Link>
-        </li>
-        <li>
-          <Link to="/about">Om Meg</Link>
-        </li>
-        <li>
-          <Link to="/projects">Prosjekter</Link>
-        </li>
-        <li>
-          <Link to="cv">CV</Link>
-        </li>
-      </ul>
-    </nav>
+    <Wrapper>
+      <StyledList>
+        <StyledListItem>
+          <StyledLink to="/">Hjem</StyledLink>
+        </StyledListItem>
+        <StyledListItem>
+          <StyledLink to="/about">Om Meg</StyledLink>
+        </StyledListItem>
+        <StyledListItem>
+          <StyledLink to="/projects">Prosjekter</StyledLink>
+        </StyledListItem>
+        <StyledListItem>
+          <StyledLink to="/cv">CV</StyledLink>
+        </StyledListItem>
+      </StyledList>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.nav`
+  background-color: var(--verdant-green);
+  padding: 1rem 2rem;
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`;
+
+const StyledListItem = styled.li`
+  margin: 0 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: var(--background-color);
+  font-weight: bold;
+  transition: color 0.3s;
+
+  &:hover {
+    color: var(--yellow-accent);
+  }
+`;
 
 export default NavBar;
