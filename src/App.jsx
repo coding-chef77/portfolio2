e2046233b93be52a6598b3967f31f082378011fc
Heine Günther <heine_gunther@hotmail.com>
2023-10-08 21:27:31 +0200

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeroPage from "./pages/HeroPage";
 import AboutPage from "./pages/AboutPage";
 import ProjectModalPage from "./pages/ProjectModalPage";
+import SingelProjectPage from "./pages/SingelProjectPage";
 import ContactPage from "./pages/ContactPage";
 import CVPage from "./pages/CV/CVPage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <h1>Hi there!!!</h1>
-      <HeroPage />
-      <AboutPage />
-      <ProjectModalPage />
-      <CVPage />
-      <ContactPage />
-    </>
+      <Routes>
+        <Route path="/" element={<HeroPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectModalPage />} />
+        <Route path="/projects/:projectId" element={<SingelProjectPage />} />
+        <Route path="/cv" element={<CVPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/SingelProjectPage.jsx
+++ src/pages/SingelProjectPage.jsx
@@ -1,4 +1,7 @@
+// import { useParams } from "react-router-dom";
+
 const SingelProjectPage = () => {
+  // const { projectId } = useParams();
   return <div>SingelProjectPage</div>;
 };
 
