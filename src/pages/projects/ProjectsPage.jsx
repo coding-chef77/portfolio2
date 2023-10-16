5c067d2d5f1bfd04b53a31330b2b51e5bbbf3bfb
Heine Günther <heine_gunther@hotmail.com>
2023-10-16 13:56:29 +0200

    Started project section

--- src/pages/projects/ProjectsPage.jsx
+++ src/pages/projects/ProjectsPage.jsx
@@ -1,25 +1,39 @@
 import styled from "styled-components";
 import { FaGithub } from "react-icons/fa";
 import image from "../../assets/web_upload.png";
+import imageTwinChat from "../../assets/twinChat.png";
 
 const ProjectsPage = () => {
   return (
     <Wrapper>
       <Card>
-        <Image src={image} alt="something" />
+        <Image src={imageTwinChat} alt="something" />
         <CardContent>
-          <Title>Project Name</Title>
-          <Description>This is a brief description of the project.</Description>
+          <Title>SoMe Project</Title>
+          <Description>
+            Dette prosjektet var min avsluttende eksamen der målet var å lage en
+            sosial medieplattform. Siden jeg selv har tvillinger, tenkte jeg at
+            det ville være en god idé å rette meg mot foreldre med tvillinger.
+            Dette er mitt første prosjekt ved bruk av Material UI, og jeg likte
+            fleksibiliteten. Vennligst klikk enten på GitHub-linken for
+            fullstendige detaljer eller knappen nedenfor for å sjekke det ut i
+            nettleseren din. Prosjektet er basert på API-dokumentasjonen levert
+            av Noroff og inkluderer funksjoner som brukerregistrering,
+            innlogging, profilhåndtering, oppretting av innlegg, oppretting av
+            kommentarer og mer.
+          </Description>
           <Links>
             <a
-              href={"https://github.com/your_username/project_name"}
+              href={
+                "https://github.com/Noroff-FEU-Assignments/project-exam-2-heine5150.git"
+              }
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaGithub size={24} />
             </a>
             <ProjectLink
-              href={"https://deployed_project_link.com"}
+              href={"https://exam-2-twin-chat.vercel.app/"}
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -113,6 +127,8 @@ const Wrapper = styled.div`
 
 const Card = styled.div`
   border: 1px solid #e1e4e8;
+  max-width: 300px;
+  min-width: 200px;
   border-radius: 8px;
   overflow: hidden;
   transition: transform 0.2s;
@@ -125,7 +141,7 @@ const Card = styled.div`
 const Image = styled.img`
   width: 100%;
   height: 200px;
-  object-fit: cover;
+  object-fit: fill;
 `;
 
 const CardContent = styled.div`
